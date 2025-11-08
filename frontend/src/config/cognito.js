ebdcaf4a84405c4ee5c11cce2a3eb65bd5205e9e
Xipog77 <superxipog77@gmail.com>
2025-11-08 11:52:16 +0700

    update cognito

--- frontend/src/config/cognito.js
+++ frontend/src/config/cognito.js
@@ -1,12 +1,12 @@
 const cognitoConfig = {
-  region: process.env.REACT_APP_AWS_REGION || 'us-east-1',
-  userPoolId: process.env.REACT_APP_USER_POOL_ID || 'your-user-pool-id',
-  userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID || 'your-client-id',
+  region: process.env.REACT_APP_AWS_REGION || 'ap-southeast-1',
+  userPoolId: process.env.REACT_APP_USER_POOL_ID || 'ap-southeast-1_tJBQES8Dg',
+  userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID || '64pcsktp7q8v369bo03j0cfrhd',
   oauth: {
-    domain: process.env.REACT_APP_COGNITO_DOMAIN || 'your-domain.auth.us-east-1.amazoncognito.com',
+    domain: process.env.REACT_APP_COGNITO_DOMAIN || 'https://ap-southeast-1tjbqes8dg.auth.ap-southeast-1.amazoncognito.com/login?client_id=64pcsktp7q8v369bo03j0cfrhd&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fcaroud.click',
     scope: ['email', 'openid', 'profile'],
-    redirectSignIn: process.env.REACT_APP_REDIRECT_SIGN_IN || 'http://localhost:3000/',
-    redirectSignOut: process.env.REACT_APP_REDIRECT_SIGN_OUT || 'http://localhost:3000/',
+    redirectSignIn: process.env.REACT_APP_REDIRECT_SIGN_IN || 'https://caroud.click',
+    redirectSignOut: process.env.REACT_APP_REDIRECT_SIGN_OUT || 'https://caroud.click',
     responseType: 'code'
   }
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from "react-oidc-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_pa1dGg56I",
+  client_id: "6k0219tpbactikiqraom2v6mgo",
+  redirect_uri: "https://caroud.click",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
